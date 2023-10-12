e865889a3ed3e9df8aa92d72d66925de4a1a87fc
rugayyaabdulla <bd7pp9q6u@code.edu.az>
2023-10-12 22:09:37 +0400

    file created

---
+++ js-2/app.js
@@ -0,0 +1,105 @@
+// TASK 1
+// let num = 25;
+// let lastNum = num % 10;
+// let secondNum = parseInt(num / 10) % 10
+// let firstNum = parseInt(num / 100)
+
+// if (lastNum == secondNum || ( secondNum == firstNum) || (lastNum == firstNum)) {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+// TASK 2
+// let a = 9;
+// let b = 10;
+// let c = 18;
+
+// if (a > b && a > c) {
+//   console.log(a);
+// } else if (b > a && b > c) {
+//   console.log(b);
+// } else if (c > a && c > b) {
+//   console.log(c);
+// } else {
+//   console.log("invalid");
+// }
+
+// TASK 3
+// let a = 6;
+// let b = 3;
+// let c = 8;
+
+// if (a > 5 && b > 5 && c > 5) {
+//   console.log(a + b + c);
+// } else if (a > 5 && b > 5) {
+//   console.log(a + b);
+// } else if (b > 5 && c > 5) {
+//   console.log(b + c);
+// } else if (a > 5 && c > 5) {
+//   console.log(a + c);
+// } else {
+//   console.log("invalid");
+// }
+
+
+// TASK 5
+// let a = 84
+// if ((a % 4 == 0) && (a % 3 == 0) && (a % 7 == 0)){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// Task 6
+// let season = 6;
+
+// switch (season) {
+//   case 12:
+//   case 1:
+//   case 2:
+//     season = "Winter";
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     season = "Spring";
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     season = "Summer";
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     season = "Autumn";
+//     break;
+//   default:
+//     season = "Invalid Month";
+// }
+
+// console.log(season);
+
+// TASK 7
+
+// let a = 3;
+// let b = 2;
+// let c = 8;
+
+// if (a > b && a > c && b < c) {
+//   console.log((a + b) / 2);
+// } else if (a > b && a > c && c < b) {
+//   console.log((a + c) / 2);
+// } else if (b > a && b > c && a < c) {
+//   console.log((b + a) / 2);
+// } else if (b > a && b > c && c < a) {
+//   console.log((b + c) / 2);
+// } else if (c > a && c > b && a < b) {
+//   console.log((c + a) / 2);
+// } else if (c > a && c > b && b < a) {
+//   console.log((c + b) / 2);
+// } else {
+//   console.log("invalid");
+// }
